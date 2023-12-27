75fe0ffb2fd802b5ad5fb6e12f80411a5a5b57b5
SergTa <tabakar456@mail.ru>
2023-12-28 01:21:27 +0300

    Added automatical switch en-ru translater

--- main_project.py
+++ main_project.py
@@ -1,52 +1,60 @@
 from transformers import pipeline
-from transformers import AutoTokenizer
+#from transformers import AutoTokenizer
 import torch
 import tensorflow
 import streamlit as st
 import io
-#from pydantic import BaseModel    # импорт ограничителя ввода данных
-from googletrans import Translator
+import chardet #детектор языка и кодировки
 
 #text = 'I can fly'
-st.title("Переводчик текстов на русский")
+st.title("Переводчик с английского на русский и наоборот")
 
 # Функиця get_data() предоставляет пользователю ввести текст в предложенное поле
 def get_data():
-  text = st.text_area("Введите текст")
-  # Проверка введенного текста на принадлежность к английскому языку
-  detector = Translator()
-  dec_lan = detector.detect(text)
+  text = st.text_area("Введите текст на русском или на английском языке")
+  return text
+
+# Проверка введенного текста на принадлежность к русскому языку
+def dec_lan (text):
+  dec_lan = chardet.detect(text.encode('cp1251'))
   
-  return text, dec_lan
-
-
+  return dec_lan['language']
 
             
-@st.cache_resource
-def tr_model(text):
+@st.cache_resource # Декоратор
+def tr_model_en_ru(text): # Функция вызова модели переводчика с английского на русский
   translator = pipeline("translation", 
                         model = "Helsinki-NLP/opus-mt-en-ru")
   transed = translator(text )[0]
   return transed 
 
+def tr_model_ru_en(text): # Функция вызова модели переводчика с русского на английский
+  translator = pipeline("translation", 
+                        model = "Helsinki-NLP/opus-mt-ru-en")
+  transed = translator(text )[0]
+  return transed 
 
 def main():
   # обрабатываем исключение
   try:
      
-    # Вызов функции для загрузки данных - текстового файла или ввода с клавиатуры. Результат сохраняется в переменной content
+    # Вызов функции для загрузки данных с клавиатуры. Результат сохраняется в переменной content
     content = get_data() 
        
-    result = st.button('Перевести на русский')
+    result = st.button('Перевести') # Кнопка Перевести
     if result:
-        translated = tr_model(content) 
+        d_lan = dec_lan (content) # Вызов функции определения языка, внесение введенного текста как аргумента, присвоение результата переменной
+        if d_lan == 'Russian': #Проверка не русский ли язык введен. Вызов модели русско-англ переводчика или англ-русского
+          translated = tr_model_ru_en(content)
+        else:
+          translated = tr_model_en_ru(content)
+       
+        st.write('*Результаты перевода:*')# Показать надпись
+        st.write(translated ["translation_text"])#Показать результат перевода
         
-        st.write('*Результаты перевода:*')
-        st.write(translated ["translation_text"])
-        st.write (dec_lan)
 
 
-  # Если в процессе выполнения программы возникает ValueError вследствие получения None Type - обрабатываем исключение
+  # Если в процессе выполнения программы возникает ValueError - обрабатываем исключение
   except ValueError:
     st.write("Не удалось осуществить перевод, пожалуйста, повторите попытку")
     return
