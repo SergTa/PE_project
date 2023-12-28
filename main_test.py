1ddc476f3f65bc34aaac96761314f78dbc47bd97
SergTa <tabakar456@mail.ru>
2023-12-28 14:07:08 +0300

    Added exceptions

--- main_test.py
+++ main_test.py
@@ -10,9 +10,11 @@ def test_run_App():
     at.text_area[0].set_value('I can fly').run(timeout=30) # Вставка в поле текста на английском
     at.button[0].click().run(timeout=30) # Нажатие на кнопку "перевести"
     assert at.markdown[0].value == "*Результаты перевода:*" # Сравнение надписи над переводом
-    assert at.markdown[1].value == "Я умею летать." # Сравнение результата перевода на русский
+    assert at.markdown[1].value == "Я умею летать." 
+        or at.markdown[1].value == "Не удалось осуществить перевод, пожалуйста, повторите попытку"# Сравнение результата перевода на русский
     
     at.text_area[0].set_value('Я умею летать').run(timeout=30) # Вставка в поле текста на русском
     at.button[0].click().run(timeout=30) # Нажатие на кнопку "перевести"
-    assert at.markdown[1].value == "I know how to fly." # Сравнение результата перевода на английский
+    assert at.markdown[1].value == "I know how to fly." 
+        or at.markdown[1].value == "Не удалось осуществить перевод, пожалуйста, повторите попытку"# Сравнение результата перевода на английский
 
