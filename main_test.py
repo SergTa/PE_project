e386bfe02d9271bc17c61cafd868f1b2fe7284f1
SergTa <tabakar456@mail.ru>
2023-12-28 12:28:54 +0300

    Changed timeout

--- main_test.py
+++ main_test.py
@@ -3,15 +3,15 @@ from streamlit.testing.v1 import AppTest #Импорт тест-клиента
 
 def test_run_App():
     
-    at = AppTest.from_file("main_project.py").run(timeout=10) # Запуск приложения с удлиненным таймаутом
+    at = AppTest.from_file("main_project.py").run(timeout=30) # Запуск приложения с удлиненным таймаутом
 
     assert not at.exception # Проверка, что приложение запустилось
        
-    at.text_area[0].set_value('I can fly').run(timeout=15) # Вставка в поле текста на английском
-    at.button[0].click().run(timeout=15) # Нажатие на кнопку "перевести"
+    at.text_area[0].set_value('I can fly').run(timeout=30) # Вставка в поле текста на английском
+    at.button[0].click().run(timeout=30) # Нажатие на кнопку "перевести"
     assert at.markdown[1].value == "Я умею летать." # Сравнение результата перевода на русский
     
-    at.text_area[0].set_value('Я умею летать').run(timeout=15) # Вставка в поле текста на русском
-    at.button[0].click().run(timeout=15) # Нажатие на кнопку "перевести"
+    at.text_area[0].set_value('Я умею летать').run(timeout=30) # Вставка в поле текста на русском
+    at.button[0].click().run(timeout=30) # Нажатие на кнопку "перевести"
     assert at.markdown[1].value == "I know how to fly." # Сравнение результата перевода на английский
 
